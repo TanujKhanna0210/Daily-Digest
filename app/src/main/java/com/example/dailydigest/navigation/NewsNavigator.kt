71f5c5912ca6c6d7298ecc963e23a6492032cebe
Tanuj Khanna <tanujkhanna72@gmail.com>
2024-06-24 02:17:11 +0530

    Setting up bottom navigation

--- app/src/main/java/com/example/dailydigest/navigation/NewsNavigator.kt
+++ app/src/main/java/com/example/dailydigest/navigation/NewsNavigator.kt
@@ -1,8 +1,127 @@
 package com.example.dailydigest.navigation
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.dailydigest.R
+import com.example.dailydigest.presentation.screens.BookmarkScreen
+import com.example.dailydigest.presentation.screens.DetailsScreen
+import com.example.dailydigest.presentation.screens.HomeScreen
+import com.example.dailydigest.presentation.screens.SearchScreen
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NewsNavigator() {
-    // Handle bottom navigation here
+
+    val bottomNavigationItems = remember {
+        listOf(
+            BottomNavigationItem(icon = R.drawable.ic_home, text = "Home"),
+            BottomNavigationItem(icon = R.drawable.ic_search, text = "Search"),
+            BottomNavigationItem(icon = R.drawable.ic_bookmark, text = "Bookmark")
+        )
+    }
+
+    val navController = rememberNavController()
+
+    val backStackState = navController.currentBackStackEntryAsState().value
+    var selectedItem by rememberSaveable {
+        mutableStateOf(0)
+    }
+
+    selectedItem = remember(key1 = backStackState) {
+        when (backStackState?.destination?.route) {
+            Route.HomeScreen.route -> 0
+            Route.SearchScreen.route -> 1
+            Route.BookmarkScreen.route -> 2
+            else -> 0
+        }
+    }
+
+    val isBottomBarVisible = remember(key1 = backStackState) {
+        backStackState?.destination?.route == Route.HomeScreen.route ||
+                backStackState?.destination?.route == Route.SearchScreen.route ||
+                backStackState?.destination?.route == Route.BookmarkScreen.route
+    }
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            if (isBottomBarVisible) {
+                NewsBottomNavigation(
+                    items = bottomNavigationItems,
+                    selected = selectedItem,
+                    onItemClick = { index ->
+                        when (index) {
+                            0 -> navigateToTab(
+                                navController = navController,
+                                route = Route.HomeScreen.route
+                            )
+
+                            1 -> navigateToTab(
+                                navController = navController,
+                                route = Route.SearchScreen.route
+                            )
+
+                            2 -> navigateToTab(
+                                navController = navController,
+                                route = Route.BookmarkScreen.route
+                            )
+                        }
+                    }
+                )
+            }
+        }
+    ) {
+        val bottomPadding = it.calculateBottomPadding()
+        NavHost(
+            navController = navController,
+            startDestination = Route.HomeScreen.route,
+            modifier = Modifier.padding(bottom = bottomPadding)
+        ) {
+            composable(route = Route.HomeScreen.route) {
+                HomeScreen()
+            }
+
+            composable(route = Route.SearchScreen.route) {
+                SearchScreen()
+            }
+
+            composable(route = Route.DetailsScreen.route) {
+                DetailsScreen()
+            }
+
+            composable(route = Route.BookmarkScreen.route) {
+                BookmarkScreen()
+            }
+        }
+    }
 }
+
+private fun navigateToTab(navController: NavController, route: String) {
+    navController.navigate(route = route) {
+        navController.graph.startDestinationRoute?.let { homeScreen ->
+            popUpTo(homeScreen) {
+                saveState = true
+            }
+            restoreState = true
+            launchSingleTop = true
+        }
+    }
+}
+
+
+
+
---
+++ app/src/main/java/com/example/dailydigest/presentation/screens/DetailsScreen.kt
@@ -0,0 +1,9 @@
+package com.example.dailydigest.presentation.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DetailsScreen() {
+    Text(text = "Details Screen")
+}
